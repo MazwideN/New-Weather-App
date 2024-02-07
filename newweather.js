be9b49f7296e0e647626a4c928a0b3b391b2901b
Anele Masuku <122709806+MazwideN@users.noreply.github.com>
2024-02-07 16:16:25 +0900

    API intergration

--- newweather.js
+++ newweather.js
@@ -1,9 +1,29 @@
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temp);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(cityvalue) {
+  let apiKey = "9t5ad1f99b16o30764016f75c42ec833";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityvalue}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 function displayCity(event) {
   event.preventDefault();
   let submitElement = document.querySelector("#form-submit-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = submitElement.value;
+
+  searchCity(submitElement.value);
 }
 
 let formElement = document.querySelector("#submit-form");
 formElement.addEventListener("submit", displayCity);
+searchCity("Pretoria"); //this city will be applied when the page is loaded
